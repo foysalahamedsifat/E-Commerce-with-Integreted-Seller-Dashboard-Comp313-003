d0adf281f29b5375a57b6f0e9cd90ceec03537df
Foysal Sifat <fsifat@my.centennialcollege.ca>
2025-02-14 18:15:01 -0500

    Authorization and Authentication Backend

---
+++ E-Commerce-with-Integreted-Seller-Dashboard-Comp313-003/E-Commerce-with-Integreted-Seller-Dashboard-Comp313-003/Authentication/ApplicationUser.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace JWTAuthentication.Authentication
+{
+    public class ApplicationUser: IdentityUser
+    {
+        [Required(ErrorMessage = "First Name is required")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Address is required")]
+        public string Address { get; set; }
+
+        [Required(ErrorMessage = "Postal Code is required")]
+        public string PostalCode { get; set; }
+    }
+}
---
+++ E-Commerce-with-Integreted-Seller-Dashboard-Comp313-003/E-Commerce-with-Integreted-Seller-Dashboard-Comp313-003/Authentication/RegisterModel.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JWTAuthentication.Authentication
+{
+    public class RegisterModel
+    {
+        [Required(ErrorMessage = "User Name is required")]
+        public string Username { get; set; }
+
+        [EmailAddress]
+        [Required(ErrorMessage = "Email is required")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "First Name is required")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Address is required")]
+        public string Address{ get; set; }
+
+        [Required(ErrorMessage = "Phone Number is required")]
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Postal Code is required")]
+        public string PostalCode  { get; set; }
+
+    }
+}
